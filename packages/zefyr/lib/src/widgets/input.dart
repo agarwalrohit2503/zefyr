45fe27b0956210e5cacedbbbce3d0c12d58e43cc
Rohit Agarwal <33911927+agarwalrohit2503@users.noreply.github.com>
2020-10-02 15:37:23 +0530

    Update input.dart

--- packages/zefyr/lib/src/widgets/input.dart
+++ packages/zefyr/lib/src/widgets/input.dart
@@ -9,6 +9,14 @@ typedef RemoteValueChanged = Function(
     int start, String deleted, String inserted, TextSelection selection);
 
 class InputConnectionController implements TextInputClient {
+ @override TextEditingValue get currentTextEditingValue => _lastKnownRemoteTextEditingValue; 
+
+@override AutofillScope get currentAutofillScope => null;
+
+@override
+ showAutocorrectionPromptRect(int start,int end){
+
+  }
    // AutofillScope get currentAutofillScope => null;
   InputConnectionController(this.onValueChanged)
       : assert(onValueChanged != null);
